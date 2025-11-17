bd702c5486e27e9210ad8ca0447918a7d5f9e59c
Qinloren <2336249419@qq.com>
2025-11-18 00:06:11 +0800

    初步完成标题栏布局

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
+  <a-config-provider>
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </a-config-provider>
 </template>
 
 <style>
--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -4,6 +4,33 @@
 
 <template>
   <header class="header-container">
+    <div class="header-box">
+      <div class="logo-box">
+        <img src="/images/favicon.png" alt="刷题鸡" />
+        <h2>刷题鸡</h2>
+      </div>
+      <div class="menu-box">
+        <ul class="menu">
+          <li class="menu-item">首页</li>
+          <li class="menu-item">题库</li>
+          <li class="menu-item">练习</li>
+          <li class="menu-item">模拟考试</li>
+        </ul>
+      </div>
+      <div class="user-box">
+        <div class="search-form">
+        </div>
+        <div class="user-info">
+          <div class="user-info-inner">
+            <img src="/images/avatar.png" class="user-basic" alt="AVATAR">
+            <span class="user-basic">测试用户</span>
+            <i class="i-mdi:keyboard-arrow-down user-basic"></i>
+            <i class="i-mdi:menu mobile-menu-icon"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mobile-menu-box"></div>
   </header>
 </template>
 
