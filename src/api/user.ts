34d51a5e3dd275569563fb3ac37f835078dea403
Qinloren <2336249419@qq.com>
2025-11-23 22:40:57 +0800

    增加用户注册、验证登录状态后端接口对接

--- src/api/user.ts
+++ src/api/user.ts
@@ -5,13 +5,13 @@ import http from '@/util/http.ts'
  * @param username 用户名
  * @param email 邮箱地址
  * @param password 密码
- * @param persistent 是否持久化
+ * @param remember 是否持久化
  */
 export const userLogin = (
   username: string | null,
   email: string | null,
   password: string,
-  persistent: boolean
+  remember: boolean
 ) => {
   if (username === email) {
     return Promise.reject('用户名与邮箱不能同时为空')
@@ -21,6 +21,34 @@ export const userLogin = (
     username,
     email,
     password,
-    persistent
+    persistent: remember
   })
+}
+
+/**
+ * 用户注册
+ * @param username 用户名
+ * @param email 邮箱地址
+ * @param avatar 头像
+ * @param password
+ */
+export const userRegister = (
+  username: string | null,
+  email: string | null,
+  avatar: string | null,
+  password: string
+) => {
+  return http.post('/sys/user/register', {
+    username,
+    email,
+    avatar,
+    password
+  })
+}
+
+/**
+ * 验证用户登录状态是否过期
+ */
+export const userVerify = () => {
+  return http.get('/sys/user/validate')
 }
