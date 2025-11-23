df9c52311500fedd0f722af84f4f0b052fd6609a
Qinloren <2336249419@qq.com>
2025-11-23 22:42:11 +0800

    配置后端服务域名跨域

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,15 @@ export default defineConfig(() => {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:32223',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      }
     }
   }
 })
