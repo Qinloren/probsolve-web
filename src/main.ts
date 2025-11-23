7d29eeb5878c1155d40b5a0dad0b55ca8a3c3a89
Qinloren <2336249419@qq.com>
2025-11-23 22:38:48 +0800

    全局引入Antdv组件库

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import App from "./App.vue";
 import router from "./router";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import "virtual:uno.css";
+import Antdv from "ant-design-vue"
+import "ant-design-vue/dist/reset.css";
 
 const app = createApp(App);
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 app.use(pinia);
+app.use(Antdv);
 app.use(router);
 
 app.mount("#app");
