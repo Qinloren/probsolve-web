87722aeef2142699506ab50779366bd3ff4661a3
Qinloren <2336249419@qq.com>
2025-11-23 22:41:35 +0800

    调整验证登录状态功能

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,10 +1,12 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { UserType } from '@/types/user.type.ts'
+import { userVerify } from '@/api/user.ts'
 
 export const useUserStore = defineStore('user', () => {
-  const userToken = ref<string>('')
+  const userToken = ref<string>()
   const userInfo = ref<UserType>()
+  const logined = ref<boolean>()
 
   const setToken = (token: string) => {
     userToken.value = token
@@ -22,11 +24,41 @@ export const useUserStore = defineStore('user', () => {
     return userInfo.value
   }
 
+  const setLoginStatus = (status: boolean) => {
+    logined.value = status
+  }
+
+  const isLogin = () => {
+    return logined.value === undefined ? false : logined.value
+  }
+
+  const checkLoginToken = async () => {
+    const response = await userVerify()
+    if (response.data.code !== 0 && !response.data.data) {
+      logout();
+      return false;
+    }
+    return true;
+  }
+
+  const logout = () => {
+    userToken.value = '';
+    userInfo.value = {} as UserType;
+    logined.value = false;
+  }
+
   return {
+    logined,
+    userInfo,
+    userToken,
     getToken,
     getInfo,
     setToken,
-    setInfo
+    setInfo,
+    setLoginStatus,
+    isLogin,
+    logout,
+    checkLoginToken
   }
 }, {
   persist: true
