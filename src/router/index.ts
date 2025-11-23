c9ce477321c9d31e22d3e87fe91eb8676d5a0e0c
Qinloren <2336249419@qq.com>
2025-11-23 22:37:32 +0800

    调整路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { message } from 'ant-design-vue'
+// import { useUserStore } from '@/stores/user.ts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,10 +16,74 @@ const router = createRouter({
           path: "home",
           name: "home",
           component: () => import("@/views/HomeView.vue"),
+        },
+        {
+          path: "user/login",
+          name: "user-login",
+          component: () => import("@/views/LoginView.vue"),
+        },
+        {
+          path: "user/register",
+          name: "user-register",
+          component: () => import("@/views/RegisterView.vue"),
+        },
+        {
+          path: "user/profile",
+          name: "user-profile",
+          meta: {
+            requiresAuth: true
+          },
+          component: () => import("@/views/ProfileView.vue"),
+        },
+        {
+          path: "user/question/lib",
+          name: "user-question-lib",
+          meta: {
+            requiresAuth: true
+          },
+          component: () => import("@/views/QuestionLibView.vue"),
         }
       ]
     }
   ],
 });
+
+router.beforeEach(async (to, from, next) => {
+  const { useUserStore } = await import("@/stores/user.ts");
+  const userStore = useUserStore();
+  if (to.meta.requiresAuth) {
+    const isValid = await userStore.checkLoginToken();
+    if (!isValid) {
+      message.error("请先登录后在操作！")
+      next({
+        name: "user-login",
+        query: {
+          redirect: to.fullPath
+        }
+      })
+      return
+    }
+  }
+  next()
+  // if (to.name && to.name.toString().startsWith("user-")) {
+//     if (!userStore.isLogin()) {
+//       notification.warning({
+//         message: "请先登录",
+//         description: "登录后即可访问该页面。",
+//         duration: 2,
+//       });
+//       next({
+//         name: "user-login",
+//         query: {
+//           redirect: to.fullPath,
+//         }
+//       })
+//       return false;
+//     } else {
+//       next();
+//     }
+//   }
+//   return true;
+})
 
 export default router;
